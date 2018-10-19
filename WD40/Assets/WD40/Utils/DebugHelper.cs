08176975b9e1fd1ab400ac726df1dd3c3ff342b7
Zentner <azentner42@gmail.com>
2018-10-19 11:44:45 +0300

    Add debug helper; Add project settings to gitignore.

---
+++ WD40/Assets/WD40/Utils/DebugHelper.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WD40
+{
+	public class DebugHelper 
+	{
+		public static void LogWarning(string text)
+		{
+			#if UNITY_EDITOR
+			Debug.LogWarning (text);
+			#endif
+		}
+		
+		public static void LogWarning(object obj)
+		{
+			#if UNITY_EDITOR
+			Debug.LogWarning (obj.ToString());
+			#endif
+		}
+
+		public static void LogError(string text)
+		{
+			#if UNITY_EDITOR
+			Debug.LogWarning (text);
+			#endif
+		}
+
+		public static void LogError(object obj)
+		{
+			#if UNITY_EDITOR
+			Debug.LogWarning (obj);
+			#endif
+		}
+
+        public static void DebugBreak ()
+        {
+			#if UNITY_EDITOR
+            Debug.Break();
+			#endif
+        }
+    }
+}
