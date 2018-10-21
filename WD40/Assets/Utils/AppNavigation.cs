9602724d2e602b21df2e46f0352967a265963a22
Zentner <azentner42@gmail.com>
2018-10-21 22:29:17 +0300

    Add AppNavigation helpers; Refold assets.

---
+++ WD40/Assets/Utils/AppNavigation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WD40
+{
+	public class AppNavigation : MonoBehaviour 
+	{
+		public static void Exit()
+		{		
+			#if UNITY_EDITOR
+			UnityEditor.EditorApplication.isPlaying = false;
+			#else
+			Application.Quit ();
+			#endif	
+		}
+	}
+}
