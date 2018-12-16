f715c1f7e719321b99319206e87913f58513a199
Zentner <azentner42@gmail.com>
2018-12-16 12:07:55 +0200

    Add multiple instances creation feature to SO utility.

--- WD40/Assets/Utils/ScriptableObjects/Editor/ScriptableObjectUtility.cs
+++ WD40/Assets/Utils/ScriptableObjects/Editor/ScriptableObjectUtility.cs
@@ -3,42 +3,54 @@ using System.Collections.Generic;
 using UnityEditor;
 using System.IO;
 
-[CustomEditor(typeof(MonoScript))]
-public sealed class MonoScriptInspector : Editor
+namespace WD40
 {
-	private MonoScript monoScript;
-    string newName = "new Object Instance";
- 
-	void OnEnable()
-	{        
-        monoScript = (MonoScript)target;
-        newName = "new "+monoScript.name + " Instance";
+    [CustomEditor(typeof(MonoScript))]
+    public sealed class ScriptableObjectUtility : Editor
+    {
+        private MonoScript monoScript;
+        string newName = "new Object Instance";
+        int newInstanceCount = 1;
+
+        void OnEnable()
+        {
+            monoScript = (MonoScript)target;
+            newName = "new " + monoScript.name + " Instance";
+        }
+
+        public override void OnInspectorGUI()
+        {
+            MonoScript ms = target as MonoScript;
+            System.Type type = ms.GetClass();
+            if (type != null && type.IsSubclassOf(typeof(ScriptableObject)) && !type.IsSubclassOf(typeof(Editor)) && !type.IsSubclassOf(typeof(EditorWindow)))
+            {
+                newName = EditorGUILayout.TextField("New asset name", newName);
+                newInstanceCount = EditorGUILayout.IntField("new assets count", newInstanceCount);
+
+                newInstanceCount = Mathf.Clamp(newInstanceCount, 0, int.MaxValue);
+
+                serializedObject.ApplyModifiedProperties();
+
+                if (GUILayout.Button("Create Instance(s)"))
+                {
+                    for (int i = 0; i < newInstanceCount; i++)
+                    {
+                        string indexToName = i > 0 ? i.ToString() : "";
+
+                        ScriptableObject asset = ScriptableObject.CreateInstance(type);
+                        string path = AssetDatabase.GetAssetPath(monoScript);
+                        path = Path.ChangeExtension(path, ".asset");
+                        path = path.Replace(monoScript.name, (newName + indexToName));
+                        AssetDatabase.CreateAsset(asset, path);
+                        EditorGUIUtility.PingObject(asset);
+                    }
+                }
+            }
+            else
+            {
+                DrawDefaultInspector();
+            }
+        }
     }
-
-	public override void OnInspectorGUI()
-	{
-		MonoScript ms = target as MonoScript;
-		System.Type type = ms.GetClass();
-		if (type != null && type.IsSubclassOf(typeof(ScriptableObject)) && !type.IsSubclassOf(typeof(Editor)) && !type.IsSubclassOf(typeof(EditorWindow)))
-		{
-            newName = EditorGUILayout.TextField("New asset name", newName);
-           
-            serializedObject.ApplyModifiedProperties();
-
-            if (GUILayout.Button("Create Instance"))
-			{
-				ScriptableObject asset = ScriptableObject.CreateInstance(type);
-				string path = AssetDatabase.GetAssetPath(monoScript);
-                path = Path.ChangeExtension(path, ".asset");
-                path = path.Replace(monoScript.name, newName);             
-                AssetDatabase.CreateAsset(asset, path);
-				EditorGUIUtility.PingObject(asset);
-			}
-		}
-		else
-		{
-			DrawDefaultInspector();
-		}
-	}
 }
 
