c79e1f0320c5909cbfdeaa130e3db9545d5edd62
Zentner <azentner42@gmail.com>
2019-01-13 00:43:55 +0200

    Add scriptable events

---
+++ WD40/Assets/Utils/ScriptableEvents/EventListenerManager.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace WD40
+{
+    public class EventListenerManager : MonoBehaviour 
+    {
+	    public GameEventListener[] EventListeners;
+	    public GameEventListenerInt[] EventListenersInt;
+
+	    public void OnEnable()
+	    {
+		    foreach (GameEventListener eventListener in EventListeners)
+		    {
+			    eventListener.Register();
+		    }
+		    foreach (GameEventListenerInt eventListenerInt in EventListenersInt)
+		    {
+			    eventListenerInt.Register();
+		    }
+	    }
+
+	    public void OnDisable()
+	    {
+		    foreach (GameEventListener eventListener in EventListeners)
+		    {
+			    eventListener.UnRegister();
+		    }
+		    foreach (GameEventListenerInt eventListenerInt in EventListenersInt)
+		    {
+			    eventListenerInt.UnRegister();
+		    }
+	    }	
+    }
+
+    [System.Serializable]
+    public class GameEventListener
+    {
+	    public GameEvent Event;
+	    public UnityEvent Response;
+
+	    public void OnEventRaised()
+	    {
+		    Response.Invoke ();
+	    }
+
+	    public void Register ()
+	    {
+		    Event.RegisterListener (this);
+	    }
+
+	    public void UnRegister ()
+	    {
+		    Event.UnregisterListener (this);
+	    }
+    }
+
+    [System.Serializable]
+    public class GameEventListenerInt
+    {
+	    public GameEventInt Event;
+	    public UnityEventInt Response;
+
+	    public void OnEventRaised(int param)
+	    {
+		    Response.Invoke (param);
+	    }
+
+	    public void Register ()
+	    {
+		    Event.RegisterListener (this);
+	    }
+
+	    public void UnRegister ()
+	    {
+		    Event.UnregisterListener (this);
+	    }
+    }
+
+    [System.Serializable] 
+    public class UnityEventInt: UnityEvent <int>
+    {
+	
+    }
+}
---
+++ WD40/Assets/Utils/ScriptableEvents/GameEventInt.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WD40
+{
+    [CreateAssetMenu(menuName = "ScriptableEvents" ,fileName = "newEvent")]
+    public class GameEventInt : ScriptableObject 
+    {
+	    List<GameEventListenerInt> listeners = new List <GameEventListenerInt>();
+
+	    public void Raise (int param) 
+	    {	
+		    for (int i = listeners.Count - 1; i >= 0; i--)
+			    listeners [i].OnEventRaised (param);
+	    }
+
+	    public void RegisterListener (GameEventListenerInt listener) {
+		    if (!listeners.Contains (listener))
+			    listeners.Add (listener);		
+	    }
+
+	    public void UnregisterListener (GameEventListenerInt listener) 
+	    {
+		    if (listeners.Contains (listener))
+			    listeners.Remove (listener);
+	    }
+    }
+}
