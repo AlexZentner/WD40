045500d708f09c3c84d37aa18c7ca0cb1eb5e872
Zentner <azentner42@gmail.com>
2019-01-15 01:47:59 +0200

    Update 2D sprite group builder.

--- WD40/Assets/Utils/2D/GroupBuilder/SpriteGroupBuilder.cs
+++ WD40/Assets/Utils/2D/GroupBuilder/SpriteGroupBuilder.cs
@@ -6,11 +6,15 @@ using Random = UnityEngine.Random;
 
 public class SpriteGroupBuilder : MonoBehaviour 
 {
-    public bool useCurrentGroup;
+    //public bool _useCurrentGroup;
+    public string _newGroupName = "new mountains group";
+    public string _newGroupSortingLayer;
+    public bool _setNewParent = true;
+    public Transform _newGroupParent;
 
     public int _maxCount;  
 
-    public GroupComponents _groupComponents;
+    public GroupComponents [] _groupComponents;
     public GenerationStages _generationStages;
 
     public Vector2 _xRange;
@@ -21,25 +25,43 @@ public class SpriteGroupBuilder : MonoBehaviour
     {
         List<GameObject> currentGroup = new List<GameObject>();
 
+        Transform [] existingGroups = GetComponentsInChildren<Transform>();
+
+        for (int i = 0; i < existingGroups.Length; i++)
+        {
+            if(existingGroups[i].gameObject != this.gameObject)
+            existingGroups[i].gameObject.SetActive(false);
+        }
+
+        GameObject groupRoot = new GameObject();
+        groupRoot.transform.SetParent(gameObject.transform);
+        groupRoot.name = _newGroupName;
+
         for (int i = 0; i < _maxCount; i++)
         {
             GameObject go = new GameObject();
-            go.transform.SetParent(gameObject.transform);
+            go.transform.SetParent(groupRoot.transform);
             go.transform.localPosition = Vector3.zero;
-            go.AddComponent<SpriteRenderer>().sprite = _groupComponents._sprite;
+            var renderer = go.AddComponent<SpriteRenderer>();
+            int n = Random.Range(0, _groupComponents.Length);
+            renderer.sprite = _groupComponents[n]._sprite;
+            renderer.sortingLayerName = _newGroupSortingLayer;
             currentGroup.Add(go);
         }
 
-        RandomizePositions(currentGroup);
+        RandomizePositions(currentGroup, groupRoot);
     }
 
-    void RandomizePositions (List<GameObject> currentGroup)
+    void RandomizePositions (List<GameObject> currentGroup, GameObject groupRoot)
     {
-        Debug.Log(currentGroup.Count);
+       // Debug.Log(currentGroup.Count);
         for (int i = 0; i < currentGroup.Count; i++)
         {
             currentGroup[i].transform.localPosition = new Vector2(Random.Range(_xRange.x, _xRange.y), Random.Range(_yRange.x, _yRange.y));
         }
+
+        if (_setNewParent)
+            groupRoot.transform.SetParent(_newGroupParent);
     }
 
     public enum GenerationStages
@@ -52,7 +74,7 @@ public class SpriteGroupBuilder : MonoBehaviour
     public class GroupComponents
     {
         public Sprite _sprite;
-        public int _weight;
+       // public int _weight;
     }
 }
 
