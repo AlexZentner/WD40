7bde6f4632e635d4863999fb032932c2a01078eb
Zentner <azentner42@gmail.com>
2019-05-12 16:16:43 +0300

    Add vector2 extentions.

---
+++ WD40/Assets/Utils/Extensions/Vector2Extentions.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Vector2Extentions
+{
+    public static int GetRandom (this Vector2Int range)
+    {
+        return Random.Range(range.x, range.y);
+    }
+
+    public static float GetRandom (this Vector2 range)
+    {
+        return Random.Range(range.x, range.y);
+    }
+}
